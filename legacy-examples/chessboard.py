39e74848f78a06e91d97fb81cf14bf54eddffb40
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2025-08-12 14:03:38 -0700

    added config for the camera

--- legacy-examples/chessboard.py
+++ legacy-examples/chessboard.py
@@ -17,6 +17,10 @@ imgpoints = []
 
 # Start video capture
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 30)
 print("Press SPACE to capture, ESC to quit and calibrate")
 
 while True:
